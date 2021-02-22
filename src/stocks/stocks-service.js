b45b7e7adb57ab279dae24a66b0b37e0441251b0
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-22 17:27:35 +0000

    Changed knex formatting

--- src/stocks/stocks-service.js
+++ src/stocks/stocks-service.js
@@ -2,12 +2,15 @@ const StocksService = {
   getAllStocks(knex) {
     return knex.select("*").from("stock_holdings")
   },
-  insertStock(knex, ticker_symbol, recommendation_status, stock_value, posting, purchase_price) {
-    return knex
-      .insert(ticker_symbol, recommendation_status, stock_value, posting, purchase_price)
-      .into("stock_holdings")
-      .returning("*")
-      .then(rows => rows[0])
+  // insertStock(knex, ticker_symbol, recommendation_status, stock_value, posting, purchase_price) {
+  //   return knex
+  //     .insert(ticker_symbol, recommendation_status, stock_value, posting, purchase_price)
+  //     .into("stock_holdings")
+  //     .returning("*")
+  //     .then(rows => rows[0])
+  // },
+  insertStock(knex) {
+    return knex('stock_holdings').insert({ticker_symbol: ticker_symbol}, {recommendation_status: recommendation_status}, {stock_value: stock_value}, {posting: posting}, {purchase_price: purchase_price})
   },
   getById(knex, id) {
     return knex
