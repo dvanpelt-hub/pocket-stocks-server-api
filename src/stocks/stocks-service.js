69133aba6cede94591e8a3340f480812f1ce68b5
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-23 11:34:04 +0000

    Update

--- src/stocks/stocks-service.js
+++ src/stocks/stocks-service.js
@@ -10,7 +10,7 @@ const StocksService = {
   //     .then(rows => rows[0])
   // },
   insertStock(knex, ticker_symbol, recommendation_status, stock_value, posting, purchase_price) {
-    return knex('stock_holdings').insert([ ticker_symbol, recommendation_status, stock_value, posting, purchase_price]).returning("*")
+    return knex.insert([{ticker_symbol: ticker_symbol}, {recommendation_status: recommendation_status}, {stock_value: stock_value}, {posting: posting}, {purchase_price: purchase_price}]).returning("*")
   },
   getById(knex, id) {
     return knex
