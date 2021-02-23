5c9e633f4bc11b7d565ba92e57d5295f92e1fc9e
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-02-23 11:57:33 +0000

    Update

--- src/stocks/stocks-service.js
+++ src/stocks/stocks-service.js
@@ -1,6 +1,6 @@
 const StocksService = {
   getAllStocks(knex) {
-    return knex.select("*").from("stock_holdings")
+    return knex.select("*").from("stock_holdings");
   },
   // insertStock(knex, ticker_symbol, recommendation_status, stock_value, posting, purchase_price) {
   //   return knex
@@ -9,23 +9,34 @@ const StocksService = {
   //     .returning("*")
   //     .then(rows => rows[0])
   // },
-  insertStock(knex, ticker_symbol, recommendation_status, stock_value, posting, purchase_price) {
-    return knex.insert([{ticker_symbol}, {recommendation_status}, {stock_value}, {posting}, {purchase_price}]).into("stock_holdings").returning("*").then(rows => rows[0])
+  insertStock(
+    knex,
+    ticker_symbol,
+    recommendation_status,
+    stock_value,
+    posting,
+    purchase_price
+  ) {
+    return knex("stock_holdings")
+      .insert(
+        ticker_symbol,
+        recommendation_status,
+        stock_value,
+        posting,
+        purchase_price
+      )
+      .returning("*")
+      .then((rows) => rows[0]);
   },
   getById(knex, id) {
-    return knex
-      .from("stock_holdings").select("*").where("id", id).first()
+    return knex.from("stock_holdings").select("*").where("id", id).first();
   },
   deleteStock(knex, id) {
-    return knex("stock_holdings")
-      .where({ id })
-      .delete()
+    return knex("stock_holdings").where({ id }).delete();
   },
   updateStock(knex, id, newStockFields) {
-    return knex("stock_holdings")
-      .where({ id })
-      .update(newStockFields)
-  }
-}
+    return knex("stock_holdings").where({ id }).update(newStockFields);
+  },
+};
 
 module.exports = StocksService;
