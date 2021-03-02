edd488a167faf0a84d44fab7aef0b0906cc6029a
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-03-02 14:40:17 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -19,10 +19,6 @@ app.use(errorHandler);
 //Plugs stocksRouter at the "/api/stocks" route (aka the 'root' route for this)
 app.use("/api/stocks", stocksRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
